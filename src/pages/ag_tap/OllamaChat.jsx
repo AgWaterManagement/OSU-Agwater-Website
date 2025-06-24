f891a0b0e8ba539f0808f9ae7154c279e0a890fe
Jen Martin <martjen4@oregonstate.edu>
2025-06-24 10:56:55 -0700

    formats request with chat params

--- src/pages/ag_tap/OllamaChat.jsx
+++ src/pages/ag_tap/OllamaChat.jsx
@@ -6,24 +6,30 @@ import './OllamaChat.css';
 const { TextArea } = Input;
 const { Title, Paragraph } = Typography;
 
-const API_URL = "https://agwater.org:11434";
+const API_URL = "https://agwater.org:5556/LLMChat";
 
 const OllamaChat = () => {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
   const inputRef = useRef(null);
+  const [error, setError] = useState(null);
 
-  async function processUserQuery(prompt) {
+  async function processUserQuery(mssgs) {
     try {
       const response = await fetch(`${API_URL}/LLMChat/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ prompt: prompt })
+        body: JSON.stringify({ 
+          model: "llama3.2",
+          messages: mssgs,
+          stream: false 
+        })
       });
 
       // Check if the response is OK
       if (!response.ok) {
+        setError("Error fetching response from the server");  
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
@@ -34,42 +40,42 @@ const OllamaChat = () => {
       setMessages((messages) => [
         ...messages,
           {
-            id: Date.now(),
-            message: combinedResponse,
-            role: 'ai',
+            content: combinedResponse,
+            role: "assistant",
           }
       ]);
     } catch (error) {
-      console.error("Error fetching Falcon response:", error);
-      setMessages((messages) => [
-        ...messages,
-          {
-            id: Date.now(),
-            message: "Error: Unable to fetch response.",
-            role: 'ai',
-          }
+      setError("Uable to fetch response. Please try again later.");  
+      console.error("Error fetching response:", error);
+      // test
+      setMessages((messages) => [...messages,
+            {content: "Test - message from ollama", role: "assistant"}
       ]);
+      console.log(messages);
     } finally {
       setLoading(false);
     }
   }
   
   const sendMessage = async () => {
+    setError(null); // Clear previous errors
+
     if (input.trim() === "") return;
 
     const newMessage = {
-        id: Date.now(),
-        message: input.trim(),
+        content: input.trim(),
         role: 'user',
     };
 
     try {
         setMessages((messages) => [...messages, newMessage]);
+        console.log(messages)
         setInput("");
         setLoading(true);
-        await processUserQuery(input);
+        await processUserQuery(messages);
     } catch (error) {
-        console.error('Error sending message:', error);
+      setError(error.message);  
+      console.error('Error sending message:', error);
     }
 
     if (inputRef.current) {
@@ -78,7 +84,8 @@ const OllamaChat = () => {
   };
 
   const openNewConv = () => {
-    setMessages([]);
+    setMessages([]); // reset messages
+    setError(null); // clear previous errors
   };
 
   return (
@@ -93,26 +100,26 @@ const OllamaChat = () => {
           </div>
         ) : (
           <div>
-            {messages.map((msg) => (
+            {messages.map((msg, index) => (
               <div
                 className={`message-container ${
                   msg.role === "user" ? "justify-end" : "justify-start"
                 }`}
-                key={msg.id}
+                key={index}
               >
                 <div
                   className={`message ${
                     msg.role === "user" ? "user-message" : "ai-message"
                   }`}
                 >
-                  {msg.message}
+                  {msg.content}
                 </div>
               </div>
             ))}
           </div>
         )}
       </main>
-
+      {error && <div style={{ color: 'red' }}>Error: {error}</div>}
       <footer className="chat-footer">
         <div className="input-container">
           <TextArea
